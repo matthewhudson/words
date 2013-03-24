d5b2a4aa9234e8d0573201cc16c079be6fcaa452
Matthew Hudson <matt@matthewghudson.com>
2013-03-24 01:39:17 -0400

    Adding pure JavaScript example.

---
+++ examples/example.js
@@ -0,0 +1,9 @@
+var generator = new (require ('../')).WordGenerator
+
+generator.generateTree( function () {
+  var word = 'matthew'
+  console.log ('Looking up anagrams for the name "' + word + '"...')
+  var init = new Date()
+  console.log (generator.getAnagrams(word))
+  console.log ('Lookup of "' + word + '" took ' + (new Date() - init) + 'ms to complete')
+});
